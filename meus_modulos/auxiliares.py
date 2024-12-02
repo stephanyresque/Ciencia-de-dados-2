610d30f725b830616ffc9bd9cbee0cb5ea5156b8
Stephany Resque <resquestephany@gmail.com>
2024-12-02 16:40:09 -0300

    02/12/24

--- meus_modulos/auxiliares.py
+++ meus_modulos/auxiliares.py
@@ -1,12 +1,16 @@
 import pandas as pd
 
-def tabela_dist_freq(dataframe, coluna):
+def tabela_dist_freq(dataframe, coluna, coluna_frequencia = False):
 
     df_estatistica = pd.DataFrame()
 
-    df_estatistica['frequencia'] = dataframe[coluna].value_counts().sort_index()
+    if coluna_frequencia:
+        df_estatistica['frequencia'] = dataframe[coluna]
+        df_estatistica['frequencia_relativa'] = df_estatistica['frequencia'] / df_estatistica['frequencia'].sum()
+    else:
+        df_estatistica['frequencia'] = dataframe[coluna].value_counts().sort_index()
 
-    df_estatistica['frequencia_relativa'] = dataframe[coluna].value_counts(normalize=True).sort_index()
+        df_estatistica['frequencia_relativa'] = dataframe[coluna].value_counts(normalize=True).sort_index()
 
     df_estatistica['frequencia_acumulada'] = df_estatistica['frequencia'].cumsum()
 
