c373d5e43367a92ebc5a9a9c1058629aaeebc457
Stephany Resque <resquestephany@gmail.com>
2024-12-23 10:36:17 -0300

    23/12/24

--- meus_modulos/auxiliares.py
+++ meus_modulos/auxiliares.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 def tabela_dist_freq(dataframe, coluna, coluna_frequencia = False):
 
@@ -18,3 +20,38 @@ def tabela_dist_freq(dataframe, coluna, coluna_frequencia = False):
 
 
     return df_estatistica
+
+def hist_box(dataframe, coluna, intervalos = 'auto'):
+
+    fig, (ax1, ax2) = plt.subplots(
+        nrows = 2, 
+        ncols = 1, 
+        sharex=True,
+        gridspec_kw={
+            'height_ratios': (0.15, 0.85),
+            'hspace': 0.02
+        }
+    )
+    
+    sns.boxplot(
+        data = dataframe, 
+        x = coluna, 
+        showmeans = True, 
+        ax = ax1, 
+        meanline = True,
+        meanprops = {'color': 'C1', 'linewidth': 1.5},
+        medianprops = {'color': 'C2', 'linewidth': 1.5, 'linestyle':'--'},
+    )
+
+    sns.histplot(data = dataframe, x = coluna, kde = True, bins = intervalos, ax = ax2)
+
+    for ax in (ax1, ax2):
+        ax.grid(True, linestyle = '--', color = 'gray', alpha = 0.5)
+        ax.set_axisbelow(True)
+
+    ax2.axvline(dataframe[coluna].mean(), color = 'C1', linestyle = '--', label = 'Média')
+    ax2.axvline(dataframe[coluna].median(), color = 'C2', linestyle = '--', label = 'Mediana')
+    ax2.axvline(dataframe[coluna].mode()[0], color = 'C3', linestyle = '--', label = 'Moda')
+    ax2.legend()
+
+    plt.show()
